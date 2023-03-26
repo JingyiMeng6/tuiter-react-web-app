1745c4d92d9dcdfc561c52dce855f1d4aebbf5b5
JingyiMeng6 <60023395+JingyiMeng6@users.noreply.github.com>
2023-03-25 21:48:56 -0700

    add like state

--- src/tuiter/home/tuit-stats.js
+++ src/tuiter/home/tuit-stats.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import {createTuit} from "../tuits/tuits-reducer";
 
 const TuitStats = (
     {tuitStats = {
@@ -8,17 +9,27 @@ const TuitStats = (
       likes: 34,
     }}
 ) => {
+  let [liked, setLiked] = useState(tuitStats.liked);
+  let [likes, setLikes] = useState(tuitStats.likes);
+  const likeClickHandler = () => {
+    if (liked) {
+      setLikes(likes - 1);
+    } else {
+      setLikes(likes + 1);
+    }
+    setLiked(!liked);
+  }
 
   return(
       <div className="row ">
         <div className="col text-muted text-center">
           <i className="bi bi-chat"></i> {tuitStats.replies}</div>
-        <div className="col text-muted text-center"><i
-            className="bi bi-arrow-repeat"></i> {tuitStats.retuits}</div>
-        <div className="col text-muted text-center"><i
-            className={"bi " + (tuitStats.liked ? 'bi-heart-fill text-danger' : 'bi-heart')}></i> {tuitStats.likes}</div>
-        <div className="col text-muted text-center"><i
-            className="bi bi-box-arrow-up"></i></div>
+        <div className="col text-muted text-center">
+          <i className="bi bi-arrow-repeat"></i> {tuitStats.retuits}</div>
+        <div className="col text-muted text-center" onClick={likeClickHandler}>
+          <i className={"bi " + (liked ? 'bi-heart-fill text-danger' : 'bi-heart')}></i> {likes}</div>
+        <div className="col text-muted text-center">
+          <i className="bi bi-box-arrow-up"></i></div>
       </div>
   )
 }
